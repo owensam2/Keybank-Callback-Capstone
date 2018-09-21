7a382ed6c5e0dba5517838245d6b44be88615161
perfetsm <perfetsm@miamioh.edu>
2018-09-21 10:31:02 -0400

    Initial add of andriod items.

---
+++ KeyBankCallback/app/src/main/java/com/example/mmarf/keybankcallback/CallbackActivity.java
@@ -0,0 +1,25 @@
+package com.example.mmarf.keybankcallback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CallbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_callback);
+
+        Button buttonInitiateConnection = findViewById(R.id.buttonInitiateConnection);
+        buttonInitiateConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+}
