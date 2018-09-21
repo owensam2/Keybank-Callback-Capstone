daa048d0d9081f732286b1e71edae55cebb0292b
perfetsm <perfetsm@miamioh.edu>
2018-09-21 11:10:22 -0400

    Change initial Activity. Try and like pages together.

--- Android/KeyBankCallback/app/src/main/java/com/example/mmarf/keybankcallback/CallbackQuestionsActivity.java
+++ Android/KeyBankCallback/app/src/main/java/com/example/mmarf/keybankcallback/CallbackQuestionsActivity.java
@@ -1,7 +1,10 @@
 package com.example.mmarf.keybankcallback;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CallbackQuestionsActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class CallbackQuestionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_callback_questions);
+
+        Button buttonQuestionFradulent = findViewById(R.id.buttonQuestionFradulent);
+        buttonQuestionFradulent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent initiateConnectionIntent = new Intent(getApplicationContext(), CallbackActivity.class);
+                //Fire off to connection screen
+                startActivity(initiateConnectionIntent);
+            }
+        });
+
     }
 }
