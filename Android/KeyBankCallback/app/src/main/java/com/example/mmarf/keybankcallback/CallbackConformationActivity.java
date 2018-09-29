e4fd35b3a6c46898345cfb4cd693e29cb369d335
perfetsm <perfetsm@miamioh.edu>
2018-09-29 10:24:02 -0400

    Add more views. Covered the callback wait time, scheduling and conformation.

---
+++ Android/KeyBankCallback/app/src/main/java/com/example/mmarf/keybankcallback/CallbackConformationActivity.java
@@ -0,0 +1,13 @@
+package com.example.mmarf.keybankcallback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallbackConformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_callback_conformation);
+    }
+}
---
+++ Android/KeyBankCallback/app/src/main/java/com/example/mmarf/keybankcallback/CallbackScheduleActivity.java
@@ -0,0 +1,16 @@
+package com.example.mmarf.keybankcallback;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class CallbackScheduleActivity extends AppCompatActivity {
+    //See http://www.learn-android-easily.com/2013/06/datepicker-and-timepicker-dialog-in.html
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_callback_schedule);
+
+    }
+}
