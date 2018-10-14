0d2d04bd676bac08473ee13d0b7f96e1d58b3f85
perfetsm <perfetsm@miamioh.edu>
2018-10-14 19:04:54 -0400

    Start work on suggested callback schedule.

---
+++ Android/KeyBankCallback/app/src/main/java/com/example/mmarf/keybankcallback/CallbackSuggestionScheduler.java
@@ -0,0 +1,13 @@
+package com.example.mmarf.keybankcallback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallbackSuggestionScheduler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_callback_suggestion_scheduler);
+    }
+}
